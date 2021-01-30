1c4001dea20d4230a50596467d3e4b706b0635a1
Erlend Sellie <erlendsellie@gmail.com>
2021-01-30 17:32:22 +0100

    Added package for notification when a door is left open

--- gitupdate.sh
+++ gitupdate.sh
@@ -2,7 +2,7 @@
 git add .
 git status
 
-echo -n "Enter the Description for the Change: " [Minor Update]  
+echo -n "Enter the Description for the Change: "   
 read CHANGE_MSG
 
 git commit -m " ${CHANGE_MSG}"
